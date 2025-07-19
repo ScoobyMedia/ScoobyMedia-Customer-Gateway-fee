3f1350c6e89942acca92daba6bb5bff3e0fca219
FrogCF <39281740+FrogCF@users.noreply.github.com>
2025-07-19 03:11:45 +0100

    Removed Folder

---
+++ ScoobyMedia-Customer-Gateway-fee-main/gateway_fees.php
@@ -0,0 +1,99 @@
+<?php
+
+require_once __DIR__ . '/common_functions.php';
+use WHMCS\Database\Capsule;
+
+if (!defined("WHMCS")) die("This file cannot be accessed directly");
+
+function gateway_fees_config()
+{
+    $configarray = array(
+        "name" => "Gateway fees",
+        "description" => "This addon allows to charge your customer a fee about the payment gateway.",
+        "version" => "1.4",
+        "author" => "AdKyNet SAS",
+        "fields" => array(
+            "enable_logs" => array(
+                "FriendlyName" => "Enable Logs",
+                "Type" => "yesno",
+                "Description" => "Enable or disable logging.",
+                "Default" => "No"
+            ),
+            "delete_table_on_deactivation" => array(
+                "FriendlyName" => "Delete Table on Deactivation",
+                "Type" => "yesno",
+                "Description" => "Choose to delete the module's SQL table when the module is deactivated.",
+                "Default" => "No"
+            ),
+            "tax_gateway_fees" => array(
+                "FriendlyName" => "Add tax on the fees",
+                "Type" => "yesno",
+                "Description" => "Enable or disable the tax option for the gateway fees.",
+                "Default" => "No"
+            ),
+        )
+    );
+
+    $gateways = Capsule::table('tblpaymentgateways')
+        ->groupBy('gateway')
+        ->pluck('gateway');
+
+    $currencies = Capsule::table('tblcurrencies')->pluck('code');
+
+    foreach ($gateways as $gateway) {
+        foreach ($currencies as $currency) {
+            $configarray['fields']["fee_1_" . $gateway . "_" . $currency] = array(
+                "FriendlyName" => $gateway . ' ' . $currency,
+                "Type" => "text",
+                "Default" => "0.00",
+                "Description" => "$"
+            );
+            $configarray['fields']["fee_2_" . $gateway . "_" . $currency] = array(
+                "FriendlyName" => $gateway . ' ' . $currency,
+                "Type" => "text",
+                "Default" => "0.00",
+                "Description" => "%"
+            );
+        }
+    }
+
+    return $configarray;
+}
+
+function gateway_fees_activate()
+{
+    $gateways = Capsule::table('tblpaymentgateways')
+        ->groupBy('gateway')
+        ->pluck('gateway');
+
+    $currencies = Capsule::table('tblcurrencies')->pluck('code');
+
+    foreach ($gateways as $gateway) {
+        foreach ($currencies as $currency) {
+            Capsule::table('tbladdonmodules')->updateOrInsert(
+                ['module' => 'gateway_fees', 'setting' => 'fee_1_' . $gateway . '_' . $currency],
+                ['value' => '0.00']
+            );
+            log_to_file("DB Insert or Update: Module 'gateway_fees', Setting 'fee_1_" . $gateway . "_" . $currency . "', Value '0.00'");
+
+            Capsule::table('tbladdonmodules')->updateOrInsert(
+                ['module' => 'gateway_fees', 'setting' => 'fee_2_' . $gateway . '_' . $currency],
+                ['value' => '0.00']
+            );
+            log_to_file("DB Insert or Update: Module 'gateway_fees', Setting 'fee_2_" . $gateway . "_" . $currency . "', Value '0.00'");
+        }
+    }
+}
+
+function gateway_fees_deactivate()
+{
+    $deleteTableOnDeactivation = Capsule::table('tbladdonmodules')
+        ->where('module', 'gateway_fees')
+        ->where('setting', 'delete_table_on_deactivation')
+        ->value('value') === 'on';
+
+    if ($deleteTableOnDeactivation) {
+        Capsule::schema()->dropIfExists('tblgatewayfees');
+        log_to_file("Table 'tblgatewayfees' deleted on module deactivation.");
+    }
+}
